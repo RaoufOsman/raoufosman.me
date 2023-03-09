f55002119bee4179a05036c90a3a1e1895c1bda5
Rene Osman <rosman21@gmail.com>
2023-03-08 23:43:14 -0500

    fixed styles

--- components/Blog/PostPreview.tsx
+++ components/Blog/PostPreview.tsx
@@ -4,6 +4,7 @@ import Avatar from "@/components/Contentful/Avatar";
 import DateFormat from "@/components/Contentful/DateFormat";
 import CoverImage from "@/components/Blog/CoverImage";
 import MediumStylePost from "./MediumStylePost";
+import { BookmarkIcon, ShareIcon } from "@heroicons/react/24/outline";
 
 export default function PostPreview({
   tags,
@@ -25,20 +26,26 @@ export default function PostPreview({
           {excerpt}
         </div>
         <div className="flex">
-          <div className="text-xs antialiased text-gray-500 mr-5 mt-2 sm:mt-1 sm:text-sm">
+          <div className="text-sm antialiased text-gray-500 mr-5 mt-1.5 sm:mt-1">
             <DateFormat dateString={date} />
           </div>
-          {/* <div className="text-sm antialiased text-gray-500 mr-5 mt-1">
-            4 min read
-          </div> */}
-          {tags.map((t: any) => (
-            <div className="text-xs antialiased text-gray-500 mr-5" key={t.id}>
-              <div className="bg-gray-100 p-2 rounded-full">{t.name}</div>
-            </div>
-          ))}
+          <div className="hidden sm:block">
+            {tags.map((t: any) => (
+              <div
+                className="text-xs antialiased text-gray-500 mr-5"
+                key={t.id}
+              >
+                <div className="bg-gray-100 p-2 rounded-full">{t.name}</div>
+              </div>
+            ))}
+          </div>
+          <div className="flex mt-1 sm:mt-0">
+            <BookmarkIcon className="h-4 w-4 text-gray-400 mt-1 mr-1 sm:h-6 sm:w-6" />
+            <ShareIcon className="h-4 w-4 text-gray-400 mt-1 sm:h-6 sm:w-6" />
+          </div>
         </div>
       </div>
-      <div>
+      <div className="flex">
         <Image
           width={300}
           height={300}
