1049dc377e7a5d3d123ff71d348e0330aadc65ac
Rene Osman <rosman21@gmail.com>
2023-03-09 09:16:05 -0500

    added standard meta tags needs images and urls as props

--- components/Metatags.tsx
+++ components/Metatags.tsx
@@ -1,15 +1,25 @@
-import Head from 'next/head'
+import Head from "next/head";
 
 interface IMetatagProps {
-  title: string,
-  pageDescription: string,
-  additionalMetaTags?: React.ReactNode
+  title: string;
+  pageDescription: string;
+  additionalMetaTags?: React.ReactNode;
+  image?: string;
+  url?: string;
 }
 
-export default function Metatags({ title, pageDescription, additionalMetaTags }: IMetatagProps) {
+export default function Metatags({
+  title,
+  pageDescription,
+  additionalMetaTags,
+  image,
+  url,
+}: IMetatagProps) {
   return (
     <Head>
+      {/* standard */}
       <title>{title}</title>
+      <meta name="title" content={title} />
       <meta name="description" content={pageDescription} />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="apple-mobile-web-app-title" content="RO" />
@@ -17,14 +27,68 @@ export default function Metatags({ title, pageDescription, additionalMetaTags }:
       <meta name="msapplication-TileColor" content="#0c0b1c" />
       <meta name="theme-color" content="#0c0b1c" />
 
+      {/* Open Graph / Facebook */}
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content={url} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={pageDescription} />
+      <meta property="og:image" content={image} />
+
+      {/* twitter */}
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content={url} />
+      <meta property="twitter:title" content={title} />
+      <meta property="twitter:description" content={pageDescription} />
+      <meta property="twitter:image" content={image} />
       {additionalMetaTags ?? ""}
 
       <link rel="icon" href="/images/favicon.ico" />
-      <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/images/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/images/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/images/favicon-16x16.png"
+      />
       <link rel="manifest" href="/images/site.webmanifest" />
-      <link rel="mask-icon" href="/images/safari-pinned-tab.svg" color="#F2637E" />
+      <link
+        rel="mask-icon"
+        href="/images/safari-pinned-tab.svg"
+        color="#F2637E"
+      />
     </Head>
-  )
+  );
+}
+
+// <!-- Primary Meta Tags -->
+// <title>Meta Tags — Preview, Edit and Generate</title>
+// <meta name="title" content="Meta Tags — Preview, Edit and Generate">
+// <meta name="description" content="With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, Twitter and more!">
+
+// <!-- Open Graph / Facebook -->
+{
+  /* <meta property="og:type" content="website">
+<meta property="og:url" content="https://metatags.io/">
+<meta property="og:title" content="Meta Tags — Preview, Edit and Generate">
+<meta property="og:description" content="With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, Twitter and more!">
+<meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"> */
+}
+
+// <!-- Twitter -->
+{
+  /* <meta property="twitter:card" content="summary_large_image">
+<meta property="twitter:url" content="https://metatags.io/">
+<meta property="twitter:title" content="Meta Tags — Preview, Edit and Generate">
+<meta property="twitter:description" content="With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, Twitter and more!">
+<meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"></meta> */
 }
