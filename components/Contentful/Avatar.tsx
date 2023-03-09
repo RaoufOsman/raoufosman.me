14f711bcb120e7c8ec21d0d80e69125fb5745227
Raouf Osman <raoufosman@gmail.com>
2023-03-08 22:53:46 -0500

    update about page, merge rad changes for medium like blog post preview

--- components/Contentful/Avatar.tsx
+++ components/Contentful/Avatar.tsx
@@ -7,16 +7,31 @@ interface IAvatarProps {
 
 export default function Avatar({ name, picture }: IAvatarProps) {
   return (
-    <div className="flex items-center">
-      <div className="relative w-12 h-12 mr-4">
-        <Image
-          src={picture.url}
-          layout="fill"
-          className="rounded-full w-16 md:w-32 lg:w-48"
-          alt={name}
-        />
+    <div className="flex flex-row mb-2">
+      <Image
+        className="inline-block sm:h-8 sm:w-8 md:h-12 md:w-12 lg:h-15 lg:w-15 rounded-full mr-1"
+        height={100}
+        width={100}
+        src={picture.url}
+        alt={name}
+      />
+      <div className="flex mt-2">
+        <span className="text-black text-lg font-bold ml-3">
+          {name}
+        </span>
+        {/* <span className="text-gray-500 text-sm mr-1">in</span>
+        <span className="text-black text-sm font-bold">Medium Staff</span> */}
       </div>
-      <div className="text-l font-bold">{name}</div>
     </div>
+    // <div className="flex items-center">
+    //   <div className="relative w-12 h-12 mr-4">
+    //     <Image
+    //       src={picture.url}
+    //       layout="fill"
+    //       className="rounded-full w-16 md:w-32 lg:w-48"
+    //       alt={name}
+    //     />
+    //   </div>
+    // </div>
   )
 }
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,4 +1,81 @@
+import Image from 'next/image'
+import BaseLayout from "@/layouts/BaseLayout";
+import classNames from 'classnames';
+
+import portraitImage from '@/public/images/ro-portrait.jpg';
+import Link from 'next/link';
+import { Socials } from "@/components/SocialIcons";
+
+function SocialLink({ className, href, children, icon: Icon }: any) {
+  return (
+    <li className={classNames(className, 'flex')}>
+      <Link
+        href={href}
+        className="group flex text-sm font-medium text-zinc-800 transition hover:text-teal-500 dark:text-white dark:hover:text-teal-500"
+      >
+        <Icon className="h-6 w-6 flex-none fill-zinc-500 transition group-hover:fill-teal-500 dark:fill-white-500" />
+        <span className="ml-4">{children}</span>
+      </Link>
+    </li>
+  )
+}
+
+function MailIcon(props: any) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+      <path strokeLinecap="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
+    </svg>
+
+  )
+}
 
 export default function About() {
+  return (
+    <BaseLayout title="RO - About" pageDescription="About page">
+      <div className="max-w-3xl mx-auto">
+        <div className="grid grid-cols-2">
+          <div>
+            <p className="my-5">Hi, I&apos;m RO - a passionate software engineer with a strong desire to use my skills and knowledge to make a positive impact in the world. As an introvert, I&apos;ve always found it challenging to put myself out there and share my ideas with others. However, I&apos;ve realized that my voice and my unique perspective are valuable, and I&apos;m determined to overcome my shyness and show the world what I have to offer.</p>
 
+            <p className="my-5">Over the past few years, I&apos;ve honed my skills as a software engineer, working on a wide range of projects that have pushed me outside of my comfort zone. I&apos;ve had the privilege of working with a variety of clients, including the National Institutes of Health and various government agencies. Through these experiences, I&apos;ve learned the value of perseverance, hard work, and collaboration, and I&apos;m constantly seeking out new challenges and opportunities to learn and grow.</p>
+
+            <p className="my-5">As a software engineer, I&apos;m committed to using my skills to make a positive impact in the world. Whether I&apos;m developing mobile applications that make life easier for everyday people or building complex web applications that streamline business operations, I&apos;m always thinking about how my work can have a real-world impact.</p>
+            <p className="my-5">I invite you to read my posts and share your comments with me via Twitter or email. I hope to provide a platform for sharing my experiences and insights with others in the software engineering community. I want to connect with like-minded individuals who are passionate about using technology to solve real-world problems and to learn from their experiences as well. By coming out of my shell and showing the world who I am, I hope to inspire others to do the same and to make a meaningful difference in the world through software engineering.</p>
+          </div>
+          <div>
+            <div className="lg:pl-20">
+              <div className="max-w-xs px-2.5 lg:max-w-none">
+                <Image
+                  src={portraitImage}
+                  alt=""
+                  sizes="(min-width: 1024px) 32rem, 20rem"
+                  className="mt-10 aspect-square rotate-3 rounded-2xl bg-zinc-100 object-cover dark:bg-zinc-800"
+                />
+              </div>
+            </div>
+            <div className="lg:pl-20">
+              <ul role="list">
+                <>
+                  {
+                    Socials.map((s, i) => (
+                      <SocialLink href={s.href} icon={s.icon} className="mt-5" key={i}>
+                        Follow on {s.name}
+                      </SocialLink>
+                    ))
+                  }
+                  <SocialLink
+                    href="mailto:raoufosman@gmail.com"
+                    icon={MailIcon}
+                    className="mt-8 border-t border-zinc-100 pt-8 dark:border-zinc-700/40"
+                  >
+                    raoufosman@gmail.com
+                  </SocialLink>
+                </>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </BaseLayout>
+  )
 }
