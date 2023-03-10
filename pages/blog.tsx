58988e45f0b4e93748a95c4e2fc72735beab7bfb
Raouf Osman <raoufosman@gmail.com>
2023-03-10 17:43:47 -0500

    escape apostrophes

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -18,10 +18,10 @@ export default function Blog({ posts }: any) {
 
   return (
     <BlogLayout title={`RO - Blog`} description="RO Blogs page">
-      <h2 className="text-2xl pt-3">Welcome to our weekly blog series! Each day, we bring you fresh content on a specific topic to help you stay motivated and informed. Here's a brief overview of what you can expect:</h2>
-      <p className="text-lg py-2">Motivation Monday: Start your week off right with a dose of inspiration and motivation. We'll share tips on how to stay motivated, stories of success, and insights into how to overcome challenges.</p>
-      <p className="text-lg py-2">Technical Tuesday: Dive into the world of technology with our Technical Tuesday posts. We'll explore the latest tech trends, highlight new tools and technologies, and share tips and tricks for developers and engineers.</p>
-      <p className="text-lg py-2">Wisdom Wednesday: Gain insights and broaden your horizons with our Wisdom Wednesday posts. We'll share quotes from thought leaders, lessons learned from industry experts, and other pearls of wisdom to help you grow both personally and professionally.</p>
+      <h2 className="text-2xl pt-3">Welcome to our weekly blog series! Each day, we bring you fresh content on a specific topic to help you stay motivated and informed. Here&apos;s a brief overview of what you can expect:</h2>
+      <p className="text-lg py-2">Motivation Monday: Start your week off right with a dose of inspiration and motivation. We&apos;ll share tips on how to stay motivated, stories of success, and insights into how to overcome challenges.</p>
+      <p className="text-lg py-2">Technical Tuesday: Dive into the world of technology with our Technical Tuesday posts. We&apos;ll explore the latest tech trends, highlight new tools and technologies, and share tips and tricks for developers and engineers.</p>
+      <p className="text-lg py-2">Wisdom Wednesday: Gain insights and broaden your horizons with our Wisdom Wednesday posts. We&apos;ll share quotes from thought leaders, lessons learned from industry experts, and other pearls of wisdom to help you grow both personally and professionally.</p>
       <p className="text-lg py-2">Tutorial Thursday: Ready to master new skills? Our Tutorial Thursday posts are packed with step-by-step guides and how-to tutorials to help you level up your technical abilities.</p>
       <p className="text-lg py-2">Fundamental Friday: Wrap up your week with a focus on the basics. Our Fundamental Friday posts will cover essential concepts in software engineering, including coding principles, software architecture, and more.</p>
       <div className="my-10">
