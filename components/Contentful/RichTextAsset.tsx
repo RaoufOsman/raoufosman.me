291d7fe72b99e8ae4d15ad3bccf1f491618e2c92
Raouf Osman <raoufosman@gmail.com>
2023-03-10 21:06:58 -0500

    update styling for assets in rich text

--- components/Contentful/RichTextAsset.tsx
+++ components/Contentful/RichTextAsset.tsx
@@ -4,7 +4,8 @@ export default function RichTextAsset({ id, assets }: any) {
   const asset = assets?.find((asset: any) => asset.sys.id === id)
 
   if (asset?.url) {
-    return <Image src={asset.url} fill={true} alt={asset.description} />
+
+    return <Image className="h-full w-full py-4" height={1000} width={1000} src={asset.url} alt={asset.description} />
   }
 
   return null
